b5e21d703196731aa38d717f99132df3fe955599
Banuprakash <banuprakashc@yahoo.co.in>
2024-10-17 10:26:02 +0530

    React

--- customerapp/src/example/AgeComponent.jsx
+++ customerapp/src/example/AgeComponent.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
 
-export default class AgeComponent extends Component {
-    
-    shouldComponentUpdate(newProps, newState) {
-        if(this.props.age === newProps.age) {
-            return false;
-        }
-        return true;
-     }
-  render() {
+// export default class AgeComponent extends Component {
+
+//     shouldComponentUpdate(newProps, newState) {
+//         if(this.props.age === newProps.age) {
+//             return false;
+//         }
+//         return true;
+//      }
+//   render() {
+//     console.log("AgeComponent renders!!!")
+//     return (
+//       <div>
+//         Age in AgeComponent {this.props.age}
+//       </div>
+//     )
+//   }
+// }
+
+
+function AgeComponent({ age }) {
     console.log("AgeComponent renders!!!")
     return (
-      <div>
-        Age in AgeComponent {this.props.age}
-      </div>
+        <div>
+            Age in AgeComponent {age}
+        </div>
     )
-  }
 }
+
+let MemoAgeComponent = React.memo(AgeComponent);
+export default MemoAgeComponent;
--- customerapp/src/example/NameComponent.jsx
+++ customerapp/src/example/NameComponent.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 
-export default class NameComponent extends Component {
- shouldComponentUpdate(newProps, newState) {
-    if(this.props.name === newProps.name) {
-        return false;
-    }
-    return true;
- }
-  render() {
+// export default class NameComponent extends Component {
+//  shouldComponentUpdate(newProps, newState) {
+//     if(this.props.name === newProps.name) {
+//         return false;
+//     }
+//     return true;
+//  }
+//   render() {
+//     console.log("NameComponent renders!!!")
+//     return (
+//       <div>Name in NameComponent {this.props.name} </div>
+//     )
+//   }
+// }
+
+function NameComponent({name}) {
     console.log("NameComponent renders!!!")
-    return (
-      <div>Name in NameComponent {this.props.name} </div>
-    )
-  }
+  return (
+    <div>Name in NameComponent {name}</div>
+  )
 }
+
+let MemoNameComponent = React.memo(NameComponent);
+export default MemoNameComponent;
