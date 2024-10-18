750ef833cac2477299d9c585a28dd91e12642e2f
Banuprakash <banuprakashc@yahoo.co.in>
2024-10-18 10:38:21 +0530

    React ShopApp

--- shopapp/src/components/NavbarComponent.jsx
+++ shopapp/src/components/NavbarComponent.jsx
@@ -8,7 +8,7 @@ export default function NavbarComponent() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">VISA Shop</Navbar.Brand>
+        <Navbar.Brand href="/products">VISA Shop</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/products">Products</Nav.Link>
           <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
