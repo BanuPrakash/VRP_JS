a2d009320fd9804330fd2177822cc9741d439949
Banuprakash <banuprakashc@yahoo.co.in>
2024-10-24 11:33:39 +0530

    redux

--- contact-manager/src/redux/store.js
+++ contact-manager/src/redux/store.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux';
 import rootReducer from './reducers'; // imports index.js from folder reducers
 
 // configureStore instead of createStore in new API
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
