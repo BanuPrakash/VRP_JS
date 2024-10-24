18b079baf279343c5688b7805657fc29b94d7144
Banuprakash <banuprakashc@yahoo.co.in>
2024-10-24 12:12:31 +0530

    redux

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import { connect } from 'react-redux';
 import ContactView from './ContactView';
+import { type } from '@testing-library/user-event/dist/type';
+import { useState } from 'react';
 function App(props) {
+  let [email, setEmail] = useState();
+  let [name, setName] = useState();
 
+  function doSubmit() {
+    let contact = {
+      email,
+      name
+    }
+
+    props.addContact(contact);
+  }
   return (
     <div className="App">
       <h1>Hello, {props.displayName}</h1>
+
+      <form>
+        <button type='button' onClick={props.clearContacts}>Clear All</button> <br />
+        Email <input type='email' onChange={(evt) => setEmail(evt.target.value)}/> <br />
+        Name <input type='text' onChange={(evt) => setName(evt.target.value)}/> <br />
+        <button onClick={doSubmit} type='button'>Add Contact</button>
+      </form>
       {
         props.contactList && props.contactList.map(contact => <ContactView contact={contact} key={contact.email} />)
       }
@@ -28,5 +47,8 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-
+  return {
+    addContact: (contact) => dispatch({type: 'ADD_CONTACT', payload: contact}),
+    clearContacts: () => dispatch({type:'CLEAR_CONTACTS'})
+  }
 }
--- contact-manager/src/ContactView.jsx
+++ contact-manager/src/ContactView.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function ContactView() {
+export default function ContactView({contact}) {
   return (
-    <div>ContactView</div>
+    <div>
+        <h1>Contacts</h1>
+        { contact.email}, {contact.name} <button>&times;</button>
+    </div>
   )
 }
+
